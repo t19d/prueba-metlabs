98c6e1a0a3a61acfe3f84c34caf768563dcdcc09
david_tojo <david_tojo@hotmail.es>
2024-06-24 09:54:36 +0200

    feat: :sparkles: Add menu searcher

---
+++ src/app/components/menu/SearchBar/SearchBar.tsx
@@ -0,0 +1,13 @@
+import SearchIcon from "../../icons/SearchIcon";
+import "./SearchBar.css";
+
+export default function SearchBar() {
+	return (
+		<div className="searcher">
+			<label htmlFor="searcher" aria-label="search">
+				<SearchIcon />
+			</label>
+			<input id="searcher" type="search" placeholder="Search Item, Collection and Account..." />
+		</div>
+	);
+}

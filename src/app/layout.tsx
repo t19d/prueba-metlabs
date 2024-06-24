940bad2ff50770c57505add69b02b740b74262e0
david_tojo <david_tojo@hotmail.es>
2024-06-24 13:11:59 +0200

    feat: Add og metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,22 @@ import "./globals.css";
 
 export const metadata: Metadata = {
 	title: "David Tojo - Prueba técnica",
-	description: "Prueba técnica realizada por David Tojo para Metlabs",
+	description: "Prueba técnica realizada por David Tojo para Metlabs.",
+	openGraph: {
+		type: "website",
+		locale: "es_ES",
+		url: "https://prueba-metlabs.vercel.app/",
+		title: "David Tojo - Prueba técnica",
+		description: "Prueba técnica realizada por David Tojo para Metlabs.",
+		images: [
+			{
+				url: "https://prueba-metlabs.vercel.app/img/og.png",
+				width: 1200,
+				height: 630,
+				alt: "Prueba técnica",
+			},
+		],
+	},
 };
 
 export default function RootLayout({
